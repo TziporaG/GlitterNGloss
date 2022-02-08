a4eec29086158c23ef1ea9aca0797a965b9efe27
michalizak <mzakutin@student.touro.edu>
2022-02-08 12:38:35 -0500

    added header footer menu

---
+++ public_html/footer.php
@@ -0,0 +1,8 @@
+
+<HR />
+<P> &copy; 2016 - your names </P>
+<P><a href="contactus.php">Contact Us</a></P>
+any other footer information
+
+</BODY>
+</HTML>
---
+++ public_html/menu.php
@@ -0,0 +1,13 @@
+	<div class="topnav">
+  <a class="active" href="#home">Home</a>
+  <a href="#face">Face</a>
+  <a href="#eye">Eye</a>
+  <a href="#lip">Lip</a>
+  <a href="#brushes">Brushes</a>
+   <a href="#sale">Sale</a>
+   <a href="#about">About Us</a>
+ <a href="#locations">Locations</a>  
+   <a href="#consultations">Consultations</a>
+    <a href="#reviews">Reviews</a>
+    <a href="#cart">Cart</a>
+</div>
